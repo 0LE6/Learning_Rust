40238db58c615284da3f67132932ce973255f4b0
Oleg <kharenkoleg@gmail.com>
2024-03-12 22:31:17 +0100

    first commit from nvim

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+pub mod basic_exercises {
+    pub fn variables() {
+        // practice basic variables in Rust
+        let num = 69;
+        println!("Printing the integer variable --> {}", num)
+    }
+}
+
+
+fn main() {
+    // module practice
+    // every model has different exercises 
+    basic_exercises::variables();
+}
