09c457b6e970de1ca0401b8048a8bba7889fa1e3
Oleg <kharenkoleg@gmail.com>
2024-03-13 21:35:56 +0100

    new commit using git

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ pub mod basic_exercises {
         let num: i32 = 420;
         println!("printing number with i32 ==> {}", num)
     }
+
+    pub fn variables3() {
+        let string: str = "varibale";
+        println!("printing a declared variable as a string -> {}", string);
+    }
 }
 
 
@@ -16,5 +21,7 @@ fn main() {
     // module practice
     // every module has different exercises 
     //basic_exercises::variables();
-    basic_exercises::variables2();
+    //basic_exercises::variables2();
+    basic_exercises::variables3();
 }
+
