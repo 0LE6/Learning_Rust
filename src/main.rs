120335c293772a5992566a8aca638fe254bdfcb5
Oleg <kharenkoleg@gmail.com>
2024-03-17 12:41:47 +0100

    learning the bool, float and const in rust

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,29 @@ pub mod basic_exercises {
         let inmutable_string: &str = "this is an inmutable string";
         println!("\ninmutable string --> {}", inmutable_string);
     }
+
+    pub fn variables4() {
+        // here we're going to practice the constant, float and boolean variables
+        const CONST_EXAMPLE: f64 = 3.14; // pi number const
+        print!("PI const --> {}", CONST_EXAMPLE);
+
+        let float_without_type_specification = 69.0; // is a f64
+        println!("Our PI const multiple by the float_without_type_specification -> {}", float_without_type_specification * CONST_EXAMPLE);  
+
+        // this gaves error beacause you can't multiply a f64 by f32
+        let float_with_type_specification: f32 = 2.0;
+        //println!("\nOur PI const multiply by the float_with_type_specification {}", float_with_type_specification * CONST_EXAMPLE);
+
+        let mut boolean_var: bool = false;
+        if CONST_EXAMPLE == 3.14 {
+            boolean_var = true;
+            println!("Yep, we're in the if statement that confirms that our const is 3.14 -> {}", boolean_var);
+        }
+        else {
+            println!("Nop, our const is not exactly 3.14 -> {}", boolean_var);
+        }
+
+    }
 }
 
 
@@ -29,6 +52,7 @@ fn main() {
     // every module has different exercises 
     //basic_exercises::variables();
     //basic_exercises::variables2();
-    basic_exercises::variables3();
+    //basic_exercises::variables3();
+    basic_exercises::variables4();
 }
 
